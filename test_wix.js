94612971d7ea10da0065ebf29cc98053b9719461
Burak Cabadan <55558309+burakcbdn@users.noreply.github.com>
2022-02-25 20:12:59 +0300

    Update test_wix.js

--- test_wix.js
+++ test_wix.js
@@ -1,9 +1,11 @@
-class TestMintButton extends HTMLElement {
-    connectedCallback() {
-        console.log('[ TEST WIX ]')
-        window.ethereum.request({ method: "eth_accounts" });
-        this.innerHTML = '<br/><br/><br/>Hello World!';
-      }
-}
+class MyCustomElement extends HTMLElement {
+  constructor() {
+    super();
+    console.log('test 1231231');
+  }
 
-customElements.define('test-mint-button', TestMintButton);
+  connectedCallback() {
+
+  }
+}
+customElements.define('my-custom-element', MyCustomElement);
